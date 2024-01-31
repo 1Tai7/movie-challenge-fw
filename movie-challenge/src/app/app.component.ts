adb1fb28fcf18504a875731e8c561ddb157ec72e
Ilce <ilsemilena1706@gmail.com>
2024-01-31 00:27:30 -0500

    add header components

--- movie-challenge/src/app/app.component.ts
+++ movie-challenge/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './movies/components/header/header.component';
+import { SelectsComponent } from './movies/components/selects/selects.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SelectsComponent],
 })
 export class AppComponent {
   public title: string = 'movie-challenge';
--- movie-challenge/src/app/app.module.ts
+++ movie-challenge/src/app/app.module.ts
@@ -3,8 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { MoviesModule } from './movies/movies.module';
 
 @NgModule({
-  imports: [AppComponent, CommonModule, BrowserModule],
+  imports: [
+    AppComponent,
+     CommonModule,
+      BrowserModule,
+      MoviesModule, ],
 })
 export class AppModule {}

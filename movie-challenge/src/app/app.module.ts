b05bd8b79ba0e315cdebe784568fed8b8fa8e1d8
Ilce <ilsemilena1706@gmail.com>
2024-01-30 21:01:16 -0500

    add app.component

---
+++ movie-challenge/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [AppComponent, CommonModule, BrowserModule],
+})
+export class AppModule {}

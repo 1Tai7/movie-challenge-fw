3a66c79b53719c530901069268366f17e8a9ce5d
Ilce <ilsemilena1706@gmail.com>
2024-02-01 20:28:56 -0500

    deleted selects component

--- movie-challenge/src/app/movies/components/selects/selects.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'movies-selects',
-  templateUrl: './selects.component.html',
-  standalone: true,
-})
-export class SelectsComponent {}

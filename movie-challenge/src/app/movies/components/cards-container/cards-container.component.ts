ff6ea709d9ed24e11876c32d337e8c547150b829
Ilce <ilsemilena1706@gmail.com>
2024-02-08 18:18:37 -0500

    integracion de API

--- movie-challenge/src/app/movies/components/cards-container/cards-container.component.ts
+++ movie-challenge/src/app/movies/components/cards-container/cards-container.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../services/movies.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'movies-cards-container',
   templateUrl: './cards-container.component.html',
   styleUrls: ['./cards-container.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
-export class CardsContainerComponent {
+export class CardsContainerComponent implements OnInit {
+  movies: any[] = [];
 
+  constructor(private apiService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.apiService.getMovies().subscribe((data: any) => {
+      this.movies = data.results;
+      console.log(data);
+    });
+  }
 }
--- movie-challenge/src/app/movies/components/services/movies.service.ts
+++ movie-challenge/src/app/movies/components/services/movies.service.ts
@@ -1,14 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class MoviesService {
+  private apikey: string =
+    '';
+  constructor(private http: HttpClient) {}
 
+  headerDict = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+   // 'Access-Control-Allow-Headers': 'Content-Type',
+    Authorization: `Bearer ${this.apikey}`,
+  };
 
-  constructor() { }
+  requestOptions = {
+    headers: new HttpHeaders(this.headerDict),
+  };
 
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?page=2', {
+      headers: this.requestOptions.headers,
+    });
+  }
 }
-
-
-
-

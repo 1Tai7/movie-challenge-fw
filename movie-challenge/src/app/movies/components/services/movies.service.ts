eb32bf63cdd3fa83612538aaa85847d483a5d8f8
Ilce <ilsemilena1706@gmail.com>
2024-02-08 19:54:25 -0500

    cards modificate

--- movie-challenge/src/app/movies/components/services/movies.service.ts
+++ movie-challenge/src/app/movies/components/services/movies.service.ts
@@ -3,14 +3,13 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MoviesService {
-  private apikey: string =
-    '';
+  private apikey: string = localStorage.getItem('apikey') || '';
   constructor(private http: HttpClient) {}
 
   headerDict = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-   // 'Access-Control-Allow-Headers': 'Content-Type',
+    // 'Access-Control-Allow-Headers': 'Content-Type',
     Authorization: `Bearer ${this.apikey}`,
   };
 
@@ -18,9 +17,11 @@ export class MoviesService {
     headers: new HttpHeaders(this.headerDict),
   };
 
+
   getMovies() {
     return this.http.get('https://api.themoviedb.org/3/discover/movie?page=2', {
       headers: this.requestOptions.headers,
     });
   }
 }
+
